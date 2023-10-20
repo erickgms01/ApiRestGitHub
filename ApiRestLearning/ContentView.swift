d977de5e11cb37b061404bfb25b3497dca5be9e1
erickgms01 <erickderi98@gmail.com>
2023-10-20 10:20:48 -0300

    removing ZStack

--- ApiRestLearning/ContentView.swift
+++ ApiRestLearning/ContentView.swift
@@ -9,93 +9,91 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var user: GitHubUser?
-    var body: some View {
-        ZStack{
+var body: some View {
+        VStack(spacing: 20) {
             
-            VStack(spacing: 20) {
-                
-                AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit )
-                        .clipShape(Circle())
-                } placeholder: {
-                    Circle()
-                        .foregroundColor(.secondary)
-                }
-                .frame(width: 120, height: 120)
-                
-                Text(user?.login ?? "login placeholder")
-                    .bold()
-                    .font(.title)
-                
-                HStack {
-                    let follow = String(user?.followers ?? 0)
-                    let following = String(user?.following ?? 0)
-                    VStack {
-                        Text("followers")
-                        Text(follow)
-                    }
-                    
-                    VStack{
-                        Text("following")
-                       
-                        Text(following)
-                    }
-                }
-                
-                Text(user?.bio ?? "bio placeholder")
-                    .font(.system(size: 20, weight: .bold))
-                
-                
-                Spacer()
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit )
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.secondary)
             }
-            .padding(50)
-            .task {
-                do {
-                    user = try await getUser()
-                }catch GHError.invalidData{
-                    print("invalid data")
-                }catch GHError.invalidURL{
-                    print("invalid url")
-                }catch GHError.invalidResponse{
-                    print("invalid response")
-                }catch GHError.invalidFollowers{
-                    print("invalid followers")
-                }catch GHError.invalidFollowing{
-                    print("invalid following")
-                }catch {
-                    print("unexpected error")
+            .frame(width: 120, height: 120)
+            
+            Text(user?.login ?? "login placeholder")
+                .bold()
+                .font(.title)
+            
+            HStack {
+                let follow = String(user?.followers ?? 0)
+                let following = String(user?.following ?? 0)
+                VStack {
+                    Text("followers")
+                    Text(follow)
                 }
+                
+                VStack{
+                    Text("following")
+                   
+                    Text(following)
+                }
+            }
+            
+            Text(user?.bio ?? "bio placeholder")
+                .font(.system(size: 20, weight: .bold))
+            
+            
+            Spacer()
+        }
+        .padding(50)
+        .task {
+            do {
+                user = try await getUser()
+            }catch GHError.invalidData{
+                print("invalid data")
+            }catch GHError.invalidURL{
+                print("invalid url")
+            }catch GHError.invalidResponse{
+                print("invalid response")
+            }catch GHError.invalidFollowers{
+                print("invalid followers")
+            }catch GHError.invalidFollowing{
+                print("invalid following")
+            }catch {
+                print("unexpected error")
             }
         }
     }
 }
-    func getUser() async throws -> GitHubUser{
-        
-        //Pega o URL
-        let endpoint = "https://api.github.com/users/erickgms01"
-        
-        //transforma em um url conhecido
-        guard let url = URL(string: endpoint) else {
-            throw GHError.invalidURL
-        }
-        
-        //faz a chamada de dados
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            throw GHError.invalidResponse
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            return try decoder.decode(GitHubUser.self, from: data)
-        } catch {
-            throw GHError.invalidData
-        }
+
+func getUser() async throws -> GitHubUser{
+    
+    //Pega o URL
+    let endpoint = "https://api.github.com/users/erickgms01"
+    
+    //transforma em um url conhecido
+    guard let url = URL(string: endpoint) else {
+        throw GHError.invalidURL
     }
+    
+    //faz a chamada de dados
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+        throw GHError.invalidResponse
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(GitHubUser.self, from: data)
+    } catch {
+        throw GHError.invalidData
+    }
+}
 
 struct GitHubUser: Codable {
     let login: String
@@ -116,5 +114,8 @@ enum GHError: Error {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
+        ContentView()
+            .environment(\.colorScheme, .dark)
     }
 }
